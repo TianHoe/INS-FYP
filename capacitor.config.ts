7285fc47034575314c4c2f9aaaf6e99f15db9bcb
Tian Hoe <87360592+TianHoe@users.noreply.github.com>
2023-05-15 10:50:36 +0800

    Initialize project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.fyp',
+  appName: 'test-fyp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
