416caf77833f3d6e77432d9fb28d795655e61028
Tian Hoe-business <n.tianhoe@gmail.com>
2023-05-20 00:02:42 +0800

    Profile updating complete.

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { DataService, Judge } from '../services/data.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
+
 export class ProfilePage implements OnInit {
-  public userDetails = {
-    name: 'NG TIAN HOE',
-    contactNumber: '01124708399',
-    email: '1191200726@student.mmu.edu.my',
-}
+  currentUserId: string;
+  currentJudge!: Judge;
 
-  constructor() { }
+  constructor(public dataService: DataService, 
+    public authService: AuthService) {
+    this.currentUserId = authService.getUID()!;
 
-  ngOnInit() {
+    this.dataService.getJudgeByAuthId(this.currentUserId).subscribe(judge => {
+      this.currentJudge = judge[0];
+    });
   }
 
+  ngOnInit() { }
 }
